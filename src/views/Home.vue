47d90cebd89ebe4438c89327f2d6068ccdc9255d
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-02-08 22:10:08 -0500

    adds working cors dependency for backend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "It's Working!",
+    };
+  },
+  created: function () {},
+  methods: {
+    indexMovies: function () {
+      console.log("do you see bananaman");
+      axios.get("http://localhost:3000/movies").then((response) => {
+        console.log(response.data);
+        this.movies = response.data;
+      });
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>All Movies</h1>
+    <button v-on:click="indexMovies()">Load Movies</button>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>Title: {{ movie.title }}</h2>
+    </div>
+  </div>
+</template>
+<style></style>
