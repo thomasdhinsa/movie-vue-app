3346183291b9a3302163f1046036ce268c28ac1f
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-02-10 05:25:10 -0500

    adds create (not done)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@ export default {
   data: function () {
     return {
       message: "It's Working!",
+      newMovieParams: {},
     };
   },
   created: function () {},
@@ -16,32 +17,21 @@ export default {
       });
     },
     createMovie: function () {
-      var params = {
-        title: "Matilda",
-        year: 1996,
-        plot: "The film centers on the titular child prodigy, Matilda Wormwood, who develops psychokinetic abilities and uses them to deal with her disreputable family; and Agatha Trunchbull, the ruthless, oppressive, and tyrannical principal of Crunchem Hall Elementary School.",
-        director: "Danny DeVito",
-        english: true,
-      };
-      axios.post("http://localhost:3000/movies", params).then((response) => {
+      axios.post("http://localhost:3000/movies", this.newMovieParams).then((response) => {
         console.log("Created!", response.data);
+        this.movies.push(response.data);
+        this.newMovieParams = {};
       });
     },
-    movieInfo: function () {
-      axios.get("http://localhost:3000/movies/3").then((response) => {
-        console.log(response.data);
+    movieShow: function (movie) {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        console.log(movie.title);
         this.movies = response.data;
       });
     },
     movieUpdate: function () {
-      var params = {
-        title: "stuff",
-        year: 1996,
-        plot: "stuff in more stuff checking update status",
-        director: "stuff",
-        english: true,
-      };
-      axios.patch("http://localhost:3000/movies/13", params).then((response) => {
+      var params = {};
+      axios.patch("http://localhost:3000/movies", params).then((response) => {
         console.log("Updated", response.data);
       });
     },
@@ -57,18 +47,44 @@ export default {
 <template>
   <div class="home">
     <h1>Create Movie</h1>
+    <p>{{ newMovieParams }}</p>
+    <div>
+      Title:
+      <input type="text" v-model="newMovieParams.title" />
+    </div>
+    <div>
+      Year:
+      <input type="integer" v-model="newMovieParams.year" />
+    </div>
+    <div>
+      Plot:
+      <input type="text" v-model="newMovieParams.plot" />
+    </div>
+    <div>
+      Director:
+      <input type="text" v-model="newMovieParams.director" />
+    </div>
+    <div>
+      English(True/False):
+      <input type="boolean" v-model="newMovieParams.english" />
+    </div>
+    <div>
+      Imageurl:
+      <input type="text" v-model="newMovieParams.imageUrl" />
+    </div>
     <button v-on:click="createMovie()">Create Movie</button>
     <h1>All Movies</h1>
-    <button v-on:click="indexMovies()">Load Movies</button>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h1>Title: {{ movie.title }}</h1>
+      <img v-bind:src="movie.imageUrl" alt="" />
+    </div>
+    <button v-on:click="indexMovies()">Load Movie</button>
     <h1>View Movie</h1>
-    <button v-on:click="movieInfo()">Load Movie 3</button>
+    <button v-on:click="movieShow(movie)">Load Movie</button>
     <h1>Update Movie</h1>
     <button v-on:click="movieUpdate()">Update Movie</button>
     <h1>Delete Movie</h1>
     <button v-on:click="movieDelete()">Delete Movie</button>
-    <div v-for="movie in movies" v-bind:key="movie.id">
-      <h2>Title: {{ movie.title }}</h2>
-    </div>
   </div>
 </template>
 <style></style>
